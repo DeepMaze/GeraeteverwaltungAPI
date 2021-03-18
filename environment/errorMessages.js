83a6a24a4e031efbefbc7699c7f18771e9079b81
Adrian Rölleke <adrian.roelleke@gmail.com>
2021-03-18 20:12:08 +0100

    finished basic locationRoute

---
+++ environment/errorMessages.js
@@ -0,0 +1,7 @@
+const messages = {
+    dbConnectionFailure: 'Something went wrong when trying to connect to the database.',
+    invalidToken: 'The token supplied is not valid.',
+    missingData: 'Some data is missing.',
+}
+
+module.exports = messages;
--- routes/location.js
+++ routes/location.js
@@ -2,16 +2,77 @@ var express = require('express');
 var mysql = require('mysql2/promise');
 
 var checkToken = require('../helpers/checkToken');
-
-var generalConfig = require('../environment/general');
+var buildUpdateSetString = require('../helpers/buildUpdateSetString');
 var mysqlConfig = require('../environment/mysql');
+var errorMsg = require('../environment/errorMessages');
+
+
 
 var router = express.Router();
 
-
-
 router.use('/*', checkToken);
 
+router.get('/getLocationList', async (req, res, next) => {
+	var query = 'SELECT * FROM locations';
+	try {
+		var connection = await mysql.createConnection(mysqlConfig);
+		var [rows] = await connection.execute(query);
+		connection.end();
+	} catch (err) {
+		res.status(500).send({ error: errorMsg.dbConnectionFailure });
+	}
+	res.status(200).send(rows);
+});
+
+router.get('/getLocation', async (req, res, next) => {
+	if (!req.query['locationID']) { req.status(400).send({ error: errorMsg.missingData }) }
+	var query = `SELECT * FROM locations WHERE ${mysql.escape(req.query['data']['locationID'])}`;
+	try {
+		var connection = await mysql.createConnection(mysqlConfig);
+		var [rows] = await connection.execute(query);
+		connection.end();
+	} catch (err) {
+		res.status(500).send({ error: errorMsg.dbConnectionFailure });
+	}
+	res.status(200).send(rows);
+});
+
+router.post('/createLocation', async (req, res, next) => {
+	var query = 'INSERT INTO locations (Label, City, Postalcode, Street)' +
+		`VALUES (${mysql.escape(req.query['data']['label'])}, ${mysql.escape(req.query['data']['city'])}, ${mysql.escape(req.query['data']['postalcode'])}, ${mysql.escape(req.query['data']['street'])})`;
+	try {
+		var connection = await mysql.createConnection(mysqlConfig);
+		await connection.execute(query);
+		connection.end();
+	} catch (err) {
+		res.status(500).send({ error: errorMsg.dbConnectionFailure });
+	}
+	res.status(204).send();
+});
+
+router.put('/updateLocation', async (req, res, next) => {
+	var query = `UPDATE locations SET ${buildUpdateSetString(req.query['data'])} WHERE LocationID = ${mysql.escape(req.query['locationID'])}`;
+	try {
+		var connection = await mysql.createConnection(mysqlConfig);
+		await connection.execute(query);
+		connection.end();
+	} catch (err) {
+		res.status(500).send({ error: errorMsg.dbConnectionFailure });
+	}
+	res.status(204).send();
+});
+
+router.put('/deleteLocation', async (req, res, next) => {
+	var query = `DELETE FROM locations WHERE LocationID = ${mysql.escape(req.query['locationID'])}`;
+	try {
+		var connection = await mysql.createConnection(mysqlConfig);
+		await connection.execute(query);
+		connection.end();
+	} catch (err) {
+		res.status(500).send({ error: errorMsg.dbConnectionFailure });
+	}
+	res.status(204).send();
+});
 
 
 module.exports = router;
