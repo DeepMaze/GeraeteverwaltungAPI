082368dfe321c34d159ae2e9d9c4bd12718208de
Adrian Rölleke <adrian.roelleke@gmail.com>
2021-03-18 18:40:10 +0100

    added newDevice route

--- routes/write.js
+++ routes/write.js
@@ -8,17 +8,24 @@ var mysqlConfig = require('../environment/mysql');
 
 var router = express.Router();
 
-router.get('/*', checkToken);
+router.post('/*', checkToken);
 
-router.get('/', async (req, res, next) => {
-    var query = '';
+router.post('/newDevice', async (req, res, next) => {
+    var query = 'INSERT INTO devices (Label, DescriptiveInformation, SerialNumber, Manufacturer, Model, RentStart, ExpectedReturn, LocationID, PersonID)' +
+        `VALUES (${mysql.escape(req.query['data']['label'])}, ${mysql.escape(req.query['data']['descriptiveInformation'])}, ${mysql.escape(req.query['data']['SerialNumber'])}, ` +
+        `${mysql.escape(req.query['data']['Manufacturer'])}, ${mysql.escape(req.query['data']['Model'])}, ${mysql.escape(req.query['data']['RentStart'])}, ` +
+        `${mysql.escape(req.query['data']['ExpectedReturn'])}, ${mysql.escape(req.query['data']['LocationID'])}, ${mysql.escape(req.query['data']['PersonID'])})`;
     try {
         var connection = await mysql.createConnection(mysqlConfig);
         var [rows, fields] = await connection.execute(query);
+        connection.end();
     } catch (err) {
         console.error(err);
+        res.send({ error: 'Something didn´t work'});
+        res.end();
     }
-    res.send(rows);
+    res.status(204)
+    res.send();
 });
 
 module.exports = router;
