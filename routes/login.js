7815a2c057847cb8bd6dd5d02c68b35520e5bba2
Adrian Rölleke <adrian.roelleke@gmail.com>
2021-04-12 09:54:54 +0200

    small cleanup

--- routes/login.js
+++ routes/login.js
@@ -13,13 +13,11 @@ const { JsonWebTokenError } = require('jsonwebtoken');
 var router = express.Router();
 
 router.get('/login', async (req, res, next) => {
-    if (!req.query) {
+    if (!req.query.userName || !req.query.passWord) {
         res.status(400).send({ userData: null });
         return;
     }
-
     var query = `SELECT ID, PassWord_Encrypted FROM users WHERE UserName = ${mysql.escape(req.query['userName'])}`;
-
     try {
         var connection = await mysql.createConnection(mysqlConfig);
         var [rows] = await connection.execute(query);
@@ -29,12 +27,10 @@ router.get('/login', async (req, res, next) => {
         if (generalConfig.log) { createLog('loginChecked', null, { userID, ip: req.socket.remoteAddress, result: false }); }
         res.status(500).send({ error: messages.dbConnectionFailure });
     }
-
     if (!rows || rows.length == 0) {
         res.status(200).send({ userData: null });
         return;
     }
-
     try {
         var passWordResult = bcrypt.compare(req.query['passWord'], rows[0]['PassWord_Encrypted'])
     } catch (err) {
@@ -42,29 +38,27 @@ router.get('/login', async (req, res, next) => {
         if (generalConfig.log) { createLog('loginChecked', null, { userID, ip: req.socket.remoteAddress, result: false }); }
         res.status(500).send({ error: messages.dbConnectionFailure });
     }
-
     if (generalConfig.log) { createLog('loginChecked', null, { userID, ip: req.socket.remoteAddress, result: passWordResult }); }
-
-    if (passWordResult) {
-        try {
-            var userData = {
-                userID: rows[0]?.id,
-                userName: req.query['userName'],
-                token: createToken(rows[0]?.userID)
-            };
-        } catch (err) {
-            if (generalConfig.debug) { console.log("[ERROR]: ", err); }
-            res.status(500).send();
-        }
-        res.status(200).send({ userData: userData });
-    } else {
+    if (!passWordResult) {
         res.status(400).send({ userData: null });
     }
+    try {
+        var userData = {
+            userID: rows[0]['ID'],
+            userName: req.query['userName'],
+            token: createToken(rows[0]?.userID)
+        };
+        console.log("userData.userID: ", userData.userID)
+    } catch (err) {
+        if (generalConfig.debug) { console.log("[ERROR]: ", err); }
+        res.status(500).send();
+    }
+    res.status(200).send(userData);
 });
 
-router.get('/checkToken', async (req, res, next) => { 
+router.get('/checkToken', async (req, res, next) => {
     var result = await checkToken()
- });
+});
 
 
 module.exports = router;
