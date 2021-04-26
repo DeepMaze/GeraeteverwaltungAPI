3478649103be06c9a73eb777359e1122ffd1e87e
Adrian Rölleke <adrian.roelleke@gmail.com>
2021-04-26 12:23:18 +0200

    added config to routes

--- environment/routes.js
+++ environment/routes.js
@@ -1,10 +1,11 @@
 
 const routes = [
+    { path: '/config', file: './routes/config' },
+    { path: '/login', file: './routes/login' },
     { path: '/device', file: './routes/device' },
     { path: '/location', file: './routes/location' },
     { path: '/person', file: './routes/person' },
     { path: '/user', file: './routes/user' },
-    { path: '/login', file: './routes/login' },
 ];
 
 module.exports = routes;
